05dbeaabfff20b939c14d9364d331c21a1760cbc
aweiismy10 <guanbolin5@gmail.com>
2026-01-20 23:01:32 +0800

    第十七題更新

--- src/app/seventeenth/seventeenth.component.ts
+++ src/app/seventeenth/seventeenth.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import {MatRadioModule} from '@angular/material/radio';
 
 @Component({
   selector: 'app-seventeenth',
-  imports: [],
+  imports: [FormsModule,MatRadioModule],
   templateUrl: './seventeenth.component.html',
   styleUrl: './seventeenth.component.scss'
 })
 export class SeventeenthComponent {
-
+  radioData!: string;
 }
